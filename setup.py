aae0a165b5d2b1e9f8804b93da280f4f8e5cc891
Søren Brokær <root@srn.io>
2015-11-25 14:20:12 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Søren Brokær',
     author_email='root@srn.io',
     url='https://github.com/srn/selectn-py',
-    download_url='https://github.com/peterldowns/mypackage/tarball/1.0.0',
+    download_url='https://github.com/srn/selectn-py/tarball/1.0.0',
     keywords=['selectn', 'dictionary', 'dot', 'notation'],
     classifiers=[],
 )
