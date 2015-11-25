2ef85611277553127aa28e37785d3dd3b2622c74
Søren Brokær <root@srn.io>
2015-11-25 14:27:22 +0700

    remove long description

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     packages=['selectn'],
     version='1.0.1',
     description='Resolves deeply-nested dictionary properties via dot-notation',
-    long_description=open('readme.md').read(),
     author='Søren Brokær',
     author_email='root@srn.io',
     url='https://github.com/srn/selectn-py',
