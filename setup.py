742fcb2e3c7cbf7d6d740607df7c2115bd986bc9
Søren Brokær <root@srn.io>
2015-11-25 14:27:31 +0700

    version 1.0.2

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from distutils.core import setup
 setup(
     name='selectn',
     packages=['selectn'],
-    version='1.0.1',
+    version='1.0.2',
     description='Resolves deeply-nested dictionary properties via dot-notation',
     author='Søren Brokær',
     author_email='root@srn.io',
     url='https://github.com/srn/selectn-py',
-    download_url='https://github.com/srn/selectn-py/tarball/1.0.1',
+    download_url='https://github.com/srn/selectn-py/tarball/1.0.2',
     keywords=['selectn', 'dictionary', 'dot', 'notation'],
     classifiers=[],
 )
